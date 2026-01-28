371d6edefe4f09f4bcc61e66f20a08a603e99e55
parth choudhary <parthnew3@gmail.com>
2026-01-28 13:53:34 +0530

    Remove Andorid

--- android/app/src/main/java/com/walkietalkie/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.walkietalkie.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  // Disable server-side features for static export
-  trailingSlash: true,
 };
 
 export default nextConfig;
